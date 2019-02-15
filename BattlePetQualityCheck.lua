8be75420d6af214aeb674d83d0367817266a9009
datagutten <datagutten@datagutten.net>
2019-02-15 20:43:30 +0100

    Check quality of captured pets

--- BattlePetQualityCheck.lua
+++ BattlePetQualityCheck.lua
@@ -4,6 +4,9 @@ EventFrame:RegisterEvent("PET_BATTLE_OPENING_DONE")
 EventFrame:RegisterEvent('NEW_PET_ADDED')
 
 local function GetHighestOwnedPetQuality(SpeciesId)
+	if not SpeciesId then --https://www.lua.org/pil/8.5.html
+		error("GetHighestOwnedPetQuality was called with empty argument", 2)
+	end
 	local highest_quality = -1
 	for _, pet_id in LibPetJournal:IteratePetIDs() do -- Loop through the owned pets
 		local OwnedSpeciesId, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _ = C_PetJournal.GetPetInfoByPetID(pet_id)
@@ -24,23 +27,114 @@ end
 function quality_color(quality)
 	return _G.ITEM_QUALITY_COLORS[quality-1].hex
 end
+
 function quality_string(quality)
 	return _G["BATTLE_PET_BREED_QUALITY"..quality]
 end
+
 function quality_color_string(quality)
 	return string.format('%s%s|r', quality_color(quality), quality_string(quality))
 end
+
 function colorize(text, color)
 	if string.sub(color, 1,4) == '|cFF' then --TODO: Fix this
 		color = string.sub(color, 5)
 	end
 	return string.format('|cFF%s%s|r', color, text)
+end
+
+function link(guid)
+	if type(guid) ~= "string" then --https://www.lua.org/pil/8.5.html
+		error("string expected", 2)
 	end
+
+	local speciesID, customName, level, xp, maxXp, displayID, isFavorite, name, icon, petType, creatureID, sourceText, description  = C_PetJournal.GetPetInfoByPetID(guid)
+	local health, maxHealth, power, speed, quality = C_PetJournal.GetPetStats(guid)
+
+	if customName then
+		name = customName
+	end
+
+	return string.format("%s|Hbattlepet:%s:%s:%s:%s:%s:%s:%s|h[%s]|h|r",
+			quality_color(quality),
+			speciesID,
+			level,
+			quality - 1,
+			health,
+			power,
+			speed,
+			guid,
+			name
+			)
+end
+
+function GetPetBySpeciesName(name)
+	for _, owned_pet_guid in LibPetJournal:IteratePetIDs() do -- Loop through the owned pets
+		local OwnedSpeciesId, customName, _, _, _, _, _, species_name  = C_PetJournal.GetPetInfoByPetID(owned_pet_guid)
+		if species_name==name then
+			return C_PetJournal.GetPetInfoByPetID(owned_pet_guid)
+		end
+	end
+end
+
+function pet_count_check(species_id, limit, quality_limit)
+	local owned_count = 0
+	local links = {}
+	local quality_counts = {}
+	-- local OwnedSpeciesId, speciesID, customName, level, xp, maxXp, displayID, isFavorite, name, icon, petType, creatureID, sourceText, description, isWild, canBattle, tradable, unique, obtainable
+	local species_name
+	if not limit then
+		limit = 3
+	end
+	if not quality_limit then
+		quality_limit = 4
+	end
+	for _, owned_pet_guid in LibPetJournal:IteratePetIDs() do -- Loop through the owned pets
+		local OwnedSpeciesId, customName, level, xp, maxXp, displayID, isFavorite, name, icon, petType, creatureID, sourceText, description, isWild, canBattle, tradable, unique, obtainable = C_PetJournal.GetPetInfoByPetID(owned_pet_guid)
+		if isWild and OwnedSpeciesId == species_id then -- Pet has requested species
+			local _, _, _, _, quality = C_PetJournal.GetPetStats(owned_pet_guid)
+			links[owned_pet_guid] = link(owned_pet_guid)
+			species_name = name
+			if not quality_counts[quality] then
+				quality_counts[quality] = 1
+			else
+				quality_counts[quality] = quality_counts[quality] + 1
+			end
+			owned_count = owned_count + 1
+			if owned_count >= 3 then
+				break
+			end
+			-- owned_text[owned_pet_guid] = string.format('%s%s|r level %d\r\n', quality_color(owned_rarity), species_name, owned_level)
+		end
+	end
+
+	if owned_count < limit then
+		return
+	end
+	if quality_counts[quality_limit] and quality_counts[quality_limit] == 3 then --All pets have maximum quality
+		return
+	end
+
+	DEFAULT_CHAT_FRAME:AddMessage(string.format('You have %d pets of type %s, where at least one has quality below %s:',
+			owned_count, species_name, quality_color_string(quality_limit)))
+	for _, link in pairs(links) do
+		DEFAULT_CHAT_FRAME:AddMessage(link)
+	end
+end
+
+-- https://wowwiki.fandom.com/wiki/Creating_a_slash_command
+SLASH_PETQ1 = "/petq"
+SlashCmdList['PETQ'] = function(msg)
+	for species in LibPetJournal:IterateSpeciesIDs() do
+		pet_count_check(species)
+	end
+	pet_count_check(species)
+end
+
 
 EventFrame:SetScript("OnEvent", function(self, event,...)
 	if event == "PET_BATTLE_OPENING_DONE" then
 		if C_PetBattles.IsWildBattle(2,i) then
-			-- DEFAULT_CHAT_FRAME:AddMessage("|cFF5cb4f8 Battle Pet Quality checker|r")
 			DEFAULT_CHAT_FRAME:AddMessage(colorize('Battle pet quality:', '28bd00'))
 			for i=1,C_PetBattles.GetNumPets(2) do --Loop through the opponents pets
 				local rarity = C_PetBattles.GetBreedQuality(2,i)
@@ -48,8 +142,6 @@ EventFrame:SetScript("OnEvent", function(self, event,...)
 
 				local upgrade_text = ''
 				local owned_rarity = GetHighestOwnedPetQuality(SpeciesId)
-				--DEFAULT_CHAT_FRAME:AddMessage('Species: ' .. SpeciesId .. ' Quality: ' .. Quality)
-				--print('Wild quality: ' .. Quality .. ' Tame quality: ' .. HasPet)
 				if not owned_rarity then
 					-- Missing pet: [name]
 					upgrade_text = "|cFFccff00 (Not owned)|r"
@@ -66,23 +158,9 @@ EventFrame:SetScript("OnEvent", function(self, event,...)
 		end
 	elseif event == 'NEW_PET_ADDED' then
 		local CapturedPetGUID = ... --Event payload
-		local CapturedSpeciesID, _, level, xp, max, dis, fav, species_name, icon = C_PetJournal.GetPetInfoByPetID(CapturedPetGUID)
-		local owned_count = 1 --Captured pet is not counted
-		for _, owned_pet_guid in LibPetJournal:IteratePetIDs() do -- Loop through the owned pets
-			local OwnedSpeciesId, customName, owned_level  = C_PetJournal.GetPetInfoByPetID(owned_pet_guid)
-			if OwnedSpeciesId == CapturedSpeciesID then -- Pet exists in journal
-				local _, _, _, _, owned_rarity = C_PetJournal.GetPetStats(owned_pet_guid)
-				--TODO: Add link
-				-- DEFAULT_CHAT_FRAME:AddMessage(string.format('Owned: %s%s|r level %d', quality_color(owned_rarity), species_name, owned_level))
-				-- DEFAULT_CHAT_FRAME:AddMessage(string.format('|Hunit:%s|h:%s|h', owned_pet_guid, species_name))
-				owned_count = owned_count + 1
-			end
-		end
-		if owned_count == 3 then
-			--DEFAULT_CHAT_FRAME:AddMessage(string.format('You have %d pets of type %s', owned_count, species_name))
-			DEFAULT_CHAT_FRAME:AddMessage(string.format('You have 3 pets of type %s, where at least 1 has quality below %s. Consider releasing one.', species_name, quality_color_string(4)))
-		end
+		local CapturedSpeciesID = C_PetJournal.GetPetInfoByPetID(CapturedPetGUID)
+		--Pet Journal is not updated with captured pet, so we need to add it manually
+		tinsert(LibPetJournal._petids, CapturedPetGUID)
+		pet_count_check(CapturedSpeciesID)
 	end
 end)
-
-
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # BattlePetQualityCheck
 Check if the quality of wild pets in a battle is better than pets of the same species in your journal. When the addon detects a pet with better quality, it displays a message in the default chat frame. The same happens when it detects a non-collected pet.
 
+When a new pet is captured the addon will check if you have any pets of the same type and notify you if you have three pets of the same species where a least one has lower quality than rare so you can release one to make room for more rare pets.
+
 To create a complete Pet-Battle experience, also try these add-ons:
 
 
